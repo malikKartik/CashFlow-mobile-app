3f27dc8667a53b215d3694b032e01c80f19dacc8
Prerna Budhraja <prernabudhraja8@gmail.com>
2020-11-12 21:00:56 +0530

    bugs

--- src/Application/screens/Team/RoomTransactions/index.js
+++ src/Application/screens/Team/RoomTransactions/index.js
@@ -14,12 +14,26 @@ import RoomCard from './RoomCard';
 import {connect} from 'react-redux';
 import {post} from '../../../requests';
 import Button from '../../../components/Button';
+import io from 'socket.io-client';
+import url from '../../../constants/url';
+const socket = io(url, {transports: ['websocket']});
 const {height, width} = Dimensions.get('window');
 const RoomTransactions = (props) => {
   const [transactions, setTransactions] = useState([]);
   const [placeName, setPlaceName] = useState('');
+
   useEffect(() => {
     handleGetTransactions();
+  }, []);
+
+  useEffect(() => {
+    socket.on('notification', (payload) => {
+      if (
+        payload.type === 'SETTLED_A_TRANSACTION' &&
+        payload.placeId === props.placeId
+      )
+        handleGetTransactions();
+    });
   }, []);
   const navigation = useNavigation();
   const renderItem = ({item, index}) => {
@@ -33,7 +47,6 @@ const RoomTransactions = (props) => {
   const handleGetTransactions = () => {
     post({route: '/api/places/getTransactions', body: {id: props.place}})
       .then((data) => {
-        console.log(data);
         setTransactions(data.transactions);
         setPlaceName(data.name);
       })
@@ -45,6 +58,10 @@ const RoomTransactions = (props) => {
     post({route: '/api/transactions/settleTransaction', body: {id: tranId}})
       .then((data) => {
         handleGetTransactions();
+        socket.emit('notification', {
+          data: {placeId: props.place, users: props.currentTeamData.users},
+          type: 'SETTLED_A_TRANSACTION',
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -55,6 +72,10 @@ const RoomTransactions = (props) => {
     post({route: '/api/places/settleAllTransactions', body: {id: props.place}})
       .then((data) => {
         handleGetTransactions();
+        socket.emit('notification', {
+          data: {placeId: props.place, users: props.currentTeamData.users},
+          type: 'SETTLED_A_TRANSACTION',
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -96,6 +117,7 @@ const styles = StyleSheet.create({
 const mapStateToProps = (state) => {
   return {
     place: state.team.currentRoom,
+    currentTeamData: state.team.currentTeamData,
   };
 };
 
